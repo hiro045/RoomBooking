82c17c157438d3f99bf6063b80285f22e3bf6a34
TakashiHirokane <hirokanetakashi@gmail.com>
2021-11-11 23:42:08 +0900

    feat:Postメソッド作成

--- main.py
+++ main.py
@@ -24,3 +24,15 @@ app = FastAPI()
 @app.get('/')
 async def index():
     return {"message": "SUCCESS"}
+
+@app.post('/booking')
+async def create_booking(booking: Booking):
+    return {"booking": booking}
+
+@app.post('/user')
+async def create_user(user: User):
+    return {"user": user}
+
+@app.post('/room')
+async def create_room(room: Room):
+    return {"room": room}
